6a0eb796310527719c7236cf0126524114179c13
awiel <awiel@scapeler.com>
2018-05-27 00:10:42 +0200

    R script

--- knmi_10min_actual_import.R
+++ knmi_10min_actual_import.R
@@ -17,7 +17,7 @@ tustr <- strsplit(tunits$value, " ")
 tmp<-unlist(tustr)[3]
 tijd<-as.POSIXct(tmp, format="%Y-%m-%d")
 #tijd2<-tijd+t
-tijd2<-strftime((tijd+t-3600000) , "%Y-%m-%dT%H:%M:%S%z")
+tijd2<-strftime((tijd+t-3600) , "%Y-%m-%dT%H:%M:%S%z")
 
 ncvar_get(ncin, "time")
 stns <-ncvar_get(ncin, "station")
@@ -25,18 +25,14 @@ stns[2:5]
 stnsname<-ncvar_get(ncin, "stationname")
 stns[stnsname=='HOEK VAN HOLLAND AWS']
 stns[stns=='06330']
-dd[stns=='06330']  # windrichting in graden 10 minute gemiddelde
-#ff[stns=='06330']  # windspeed op 10m 10 minuten gemiddelde
+ddValue<-dd[stns=='06330']  # windrichting in graden 10 minute gemiddelde
+ffValue<-ff[stns=='06330']  # windspeed op 10m 10 minuten gemiddelde
 
 nc_close(ncin)
-dd
-ff
-tijd2
-
-
 
 foi<-paste('KNMI','06330',sep='')
-observation<-paste('apri-sensor-knmi-dd:',dd,sep='')
+observation<-paste('apri-sensor-knmi-dd:',ddValue,sep='')
+observation<-paste(observation,',','apri-sensor-knmi-ff:',ffValue,sep='')
 url1<-'https://openiod?SERVICE=WPS&REQUEST=Execute&identifier=transform_observation&action=insertom&sensorsystem=apri-sensor-knmi&offering=offering_knmi_initial&commit=true';
 url <- paste(url1,'&foi=',foi,'&observation=',observation,'&measurementTime=',tijd2,sep='');
 url
