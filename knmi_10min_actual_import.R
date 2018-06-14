5b33de922504c9161f6b2bebe7ec46c29d3cc90f
awiel <awiel@scapeler.com>
2018-06-14 08:23:40 +0200

    +ta,rh

--- knmi_10min_actual_import.R
+++ knmi_10min_actual_import.R
@@ -13,6 +13,8 @@ dd <- ncvar_get(ncin, "dd")
 ff <- ncvar_get(ncin, "ff")
 D1H <- ncvar_get(ncin, "D1H")
 R1H <- ncvar_get(ncin, "R1H")
+ta <- ncvar_get(ncin, "ta")
+rh <- ncvar_get(ncin, "rh")
 
 t <- ncvar_get(ncin, "time")
 tunits <- ncatt_get(ncin, "time", "units")
@@ -33,6 +35,8 @@ ddValue<-dd[stns=='06330']  # windrichting in graden 10 minute gemiddelde
 ffValue<-ff[stns=='06330']  # windspeed op 10m 10 minuten gemiddelde
 D1HValue<-D1H[stns=='06330']  # duration rainfall last hour
 R1HValue<-R1H[stns=='06330']  # rainfall in mm last hour
+taValue<-ta[stns=='06330']  # Air Temperature 1 Min Average
+rhValue<-rh[stns=='06330']  # Relative Humidity 1 Min Average
 
 nc_close(ncin)
 
@@ -41,6 +45,8 @@ observation<-paste('apri-sensor-knmi-dd:',ddValue,sep='')
 observation<-paste(observation,',','apri-sensor-knmi-ff:',ffValue,sep='')
 observation<-paste(observation,',','apri-sensor-knmi-D1H:',D1HValue,sep='')
 observation<-paste(observation,',','apri-sensor-knmi-R1H:',R1HValue,sep='')
+observation<-paste('apri-sensor-knmi-ta:',taValue,sep='')
+observation<-paste(observation,',','apri-sensor-knmi-rh:',rhValue,sep='')
 url1<-'https://openiod.org/SCAPE604/openiod?SERVICE=WPS&REQUEST=Execute&identifier=transform_observation&action=insertom&sensorsystem=apri-sensor-knmi10m&offering=offering_knmi10m_initial&commit=true';
 url <- paste(url1,'&foi=',foi,'&observation=',observation,'&measurementTime=',tijd2,sep='');
 url
